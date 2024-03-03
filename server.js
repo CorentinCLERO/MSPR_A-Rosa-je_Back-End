c71df67661fbf9dad309fad9264de643cd7b2d4b
pa large <pierreantoine.large@gmail.com>
2024-03-03 12:08:57 +0100

    adding user

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes, where } = require("sequelize");
+const http = require("http");
 
 const app = express();
 const port = 8080;
 
-// Configuration de Sequelize pour SQLite
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
 const sequelize = new Sequelize({
+  sync: false,
   dialect: "sqlite",
   storage: "db.sqlite",
 });
 
 const Plant = require("./models/Plant")(sequelize, DataTypes);
-const User = require("./models/User");
+const User = require("./models/User")(sequelize, DataTypes);
 
-// Synchronisation du modèle avec la base de données
 (async () => {
   try {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
     console.log("Modèles synchronisés avec la base de données.");
 
-    // Une fois la synchronisation terminée, commencez à écouter les requêtes HTTP
     app.listen(port, () => {
       console.log(`Le serveur écoute sur le port ${port}`);
     });
@@ -28,10 +31,11 @@ const User = require("./models/User");
   }
 })();
 
-// Route pour récupérer toutes les plantes depuis la base de données
 app.get("/plants", async (req, res) => {
   try {
-    console.log("sr");
+    console.log(req.body);
+    const { variety } = req.body;
+    console.log(variety);
     const plants = await Plant.findAll();
     res.json(plants);
   } catch (error) {
@@ -39,5 +43,79 @@ app.get("/plants", async (req, res) => {
     res
       .status(500)
       .send("Une erreur s'est produite lors de la récupération des plantes.");
+  }
+});
+
+app.post("/plant", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { variety } = req.body;
+    console.log(variety);
+    const plants = await Plant.findAll();
+    res.json(plants);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des plantes:", error);
+    res
+      .status(500)
+      .send("Une erreur s'est produite lors de la récupération des plantes.");
+  }
+});
+
+app.get("/requests", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { variety } = req.body;
+    console.log(variety);
+    const plants = await Plant.findAll();
+    res.json(plants);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des plantes:", error);
+    res
+      .status(500)
+      .send("Une erreur s'est produite lors de la récupération des plantes.");
+  }
+});
+
+app.get("/plantsos", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { userId } = req.body;
+
+    const user = await User.findOne();
+    console.log(user);
+    res.json(plants);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des plantes:", error);
+    res
+      .status(500)
+      .send("Une erreur s'est produite lors de la récupération des plantes.");
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const { userId, email, password, pseudo, role, firstname, lastname } =
+      req.body;
+
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (user == null) {
+      await User.create({
+        email: email,
+        password: password,
+        pseudo: pseudo,
+        role: role,
+        firstname: firstname,
+        lastname: lastname,
+      });
+      res.send("utilisateur ajouté");
+    }
+    res.send("l'utilisateur existe déjà");
+  } catch (error) {
+    console.error("Erreur lors de la création de l'utilisateur:", error);
+    res.status(500).send("Erreur lors de la création de l'utilisateur.");
   }
 });
