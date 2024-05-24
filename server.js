23473e045cf4315aecae397a02267cb99df708ec
CorentinCLERO <corentin.clero@epsi.fr>
2024-05-24 17:23:00 +0200

    👌 IMPROVE: have multiple url for cors

--- server.js
+++ server.js
@@ -6,38 +6,40 @@ dotenv.config();
 
 const app = express();
 
-// const corsOptions = {
-//   origin: process.env.FRONT_END_URL,
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-//   credentials: true,
-//   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-//   optionsSuccessStatus: 204
-// };
+const allowedOrigins = Object.keys(process.env)
+  .filter(key => key.startsWith("FRONT_END_URL"))
+  .map(key => process.env[key]);
 
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  credentials: true,
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-const allowedOrigins = [process.env.FRONT_END_URL, "https://9000-idx-msprarosajeadmin-1716367498654.cluster-blu4edcrfnajktuztkjzgyxzek.cloudworkstations.dev", "http://localhost:5173/"];
 
-app.use((req, res, next) => {
-  console.log("Request Method:", req.method, "Request Origin:", req.headers.origin); // Log la méthode et l'origine de la requête
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }  
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
-  if (req.method === "OPTIONS") {
-    console.log("Preflight Request Headers:", req.headers); // Log les en-têtes des requêtes préflight
-    return res.sendStatus(204);
-  }
-  console.log("Request Headers:", req.headers); // Log les en-têtes des autres requêtes
-  next();
-});
-
-app.get("api/admin/test", (req, res) => {
-  res.status(201).json({ message: "CORS test successful" });
-});
+// app.use((req, res, next) => {
+//   console.log("Request Method:", req.method, "Request Origin:", req.headers.origin); // Log la méthode et l'origine de la requête
+//   res.header("Access-Control-Allow-Origin", process.env.FRONT_END_URL);
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization");
+//   if (req.method === "OPTIONS") {
+//     console.log("Preflight Request Headers:", req.headers); // Log les en-têtes des requêtes préflight
+//     return res.sendStatus(204);
+//   }
+//   console.log("Request Headers:", req.headers); // Log les en-têtes des autres requêtes
+//   next();
+// });
 
 require("./routes/routesMobile.js")(app);
 require("./routes/routesAdmin.js")(app);
