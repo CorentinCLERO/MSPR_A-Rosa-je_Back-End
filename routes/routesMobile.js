8aec6c61c27305cdf01cc7303a9cf0d0131da5f1
CorentinCLERO <corentin.clero@epsi.fr>
2024-05-29 22:56:22 +0200

    👌 IMPROVE: clean routesMobile

--- routes/routesMobile.js
+++ routes/routesMobile.js
@@ -28,29 +28,19 @@ module.exports = (app) => {
   const controllerRequest = require("../controllers/controllerRequest.js");
   router.get("/requests/:userId", authRole("owner"), controllerRequest.getRequests);
   router.get("/requests", authRole("keeper"), controllerRequest.getAllRequests);
-  // router.get("/request", controllerRequest.getRequest);
   router.post("/request", authRole("owner"), controllerRequest.postRequest);
-  // router.post("/request/:requestId", controllerRequest.RequestAccept);
   router.delete("/request/:requestId", authRole("owner"), controllerRequest.deleteRequest);
-  // router.post("/post/:requestId", controllerRequest.post);
 
   //HELP REQUESTS ROUTES
   const controllerHelpRequest = require("../controllers/controllerHelpRequest.js");
   router.get("/plantsos/:userId", authRole("keeper"), controllerHelpRequest.getHelpRequests);
-  router.get("/plantsos", controllerHelpRequest.getAllHelpRequests);
-  // router.get("/plantsos/:plantSosId", controllerHelpRequest.getHelpRequestInfo);
-  router.post("/plantsos", authRole("keeper"), controllerHelpRequest.postHelpRequest); ///
-  // router.post("/plantsos/:plantSosId", controllerHelpRequest.postHelpRequestAnswer);
+  router.get("/plantsos", authRole("botanist"), controllerHelpRequest.getAllHelpRequests);
 
   //USER ROUTES
   const controllerUser = require("../controllers/controllerUser.js");
-  // router.post("/user", controllerUser.createUser);
-  // router.post("/login_user", controllerUser.loginUser);
-  // router.post("/verify_token", controllerUser.verifyToken);
   router.patch("/user/:id", authRole("owner"), controllerUser.modifyUser);
   router.delete("/user/:id", authRole("owner"), controllerUser.deleteUser);
   router.get("/pseudos", authRole("owner"), controllerUser.getAllPseudos);
-  // router.get("/adminBearerToken", controllerUser.getAdminBearerToken);
 
   //ADRESS ROUTES
   const controllerAdress = require("../controllers/controllerAdress.js");
